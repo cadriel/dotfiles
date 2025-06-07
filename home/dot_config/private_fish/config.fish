2b408dc8e69c943601c12a148b596e55467961ad
Craig Bassett <craig.bassett@gmail.com>
2025-06-06 22:19:42 -0500

    (feat) added yazi and zoxide + moved some config about

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
   # Update LSColors to match what we want.
   set -gx LS_COLORS (vivid generate gruvbox-dark)
 
-  # Init startship
+  # Init starship
   starship init fish | source
+
+  # Init zoxide
+  zoxide init fish | source
 end
---
+++ home/dot_config/private_fish/functions/cd.fish
@@ -0,0 +1,7 @@
+if test "$USER_OS" = Darwin;
+  alias cd='z'
+end
+
+if test "$USER_OS" = Linux;
+  alias cd='z'
+end
