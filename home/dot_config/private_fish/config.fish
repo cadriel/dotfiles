393ee4633f60ce2e7e2803c9ad80a6bd12367171
Craig Bassett <craig.bassett@gmail.com>
2025-12-17 12:16:54 +1300

    Minor app updates

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -1,10 +1,14 @@
 if status is-interactive
-  # Update LSColors to match what we want.
-  set -gx LS_COLORS (vivid generate catppuccin-mocha)
+    # Update LSColors to match what we want.
+    set -gx LS_COLORS (vivid generate catppuccin-mocha)
 
-  # Init starship
-  starship init fish | source
+    # Configure homebrew
+    set -gx $HOMEBREW_NO_ENV_HINTS=1
+    set -gx $HOMEBREW_AUTO_UPDATE_SECS 86400
 
-  # Init zoxide
-  zoxide init fish | source
+    # Init starship
+    starship init fish | source
+
+    # Init zoxide
+    zoxide init fish | source
 end
