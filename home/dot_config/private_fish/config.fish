5031d9f5972b0cbec3c0fe8168a963ae6ba5fff7
Craig Bassett <craig.bassett@gmail.com>
2026-01-30 17:04:56 +1300

    Updated to reflect latest fish version

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -1,10 +1,16 @@
 if status is-interactive
+    # Set default fish key bindings
+    set --global fish_key_bindings fish_default_key_bindings
+
+    # Set the correct fish theme
+    fish_config theme choose ayu-mirage
+
     # Update LSColors to match what we want.
     set -gx LS_COLORS (vivid generate catppuccin-mocha)
 
     # Configure homebrew
-    set -gx $HOMEBREW_NO_ENV_HINTS=1
-    set -gx $HOMEBREW_AUTO_UPDATE_SECS 86400
+    set -gx HOMEBREW_NO_ENV_HINTS 1
+    set -gx HOMEBREW_AUTO_UPDATE_SECS 86400
 
     # Init starship
     starship init fish | source
